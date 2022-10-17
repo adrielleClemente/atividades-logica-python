8a5a7bcbae07fab4741e989b522fc8cf6cb87600
DRIKA <adrielle.letras@gmail.com>
2022-10-17 11:03:15 -0300

    atualizacao

--- problemas-python-brasil/estrutura_sequencial.py
+++ problemas-python-brasil/estrutura_sequencial.py
@@ -52,3 +52,42 @@ print(total_do_salario)
 fahr = float(input('Quantos Fahrenheit? '))
 celsius = 5*((fahr-32)/9)
 print(int(celsius), '°C')
+
+#10. Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em
+#graus Fahrenheit.
+cel = int(input('Quantos Celsius?'))
+fahr = (cel * (9/5) + 32)
+print(int(fahr),'°F')
+
+#11. Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+    #a. O produto do dobro do primeiro com metade do segundo .
+    #b. A soma do triplo do primeiro com o terceiro.
+    #c. O terceiro elevado ao cubo.
+primeiro_int=int(input('Primeiro número inteiro: '))
+segundo_int=int(input('Segundo número inteiro: '))
+numero_real=float(input('N° Real: '))
+a = ((primeiro_int*2)*(segundo_int/2))
+b = ((primeiro_int*3)+numero_real)
+c = (numero_real**3)
+print('\nResposta letra a é {0};\nLetra b é {1};\nE da letra c é {2}.'.format(a,b,c))
+
+#12. Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule
+#seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58.
+altura=float(input('Qual seu altura? '))
+peso_ideal=((72.7*altura)-58)
+print('Seu peso ideal é: {:.3f} Kg'.format(peso_ideal))
+
+#13. Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule
+#seu peso ideal, utilizando as seguintes fórmulas:
+    # a. Para homens: (72.7*h) - 58.
+    # b. Para mulheres: (62.1*h) - 44.7.
+h=float(input('Qual sua altura? '))
+genero= str(input('Digite M para mulher ou H para homem:'))
+mulher = ((62.1*h)-44.7)
+homem = ((72.7*h)-58)
+if genero == 'm':
+    print('Peso para mulher: ',mulher)
+elif genero == 'h':
+    print('Peso para homem: ',homem)
+else:
+    print('Ambos pesos: ',mulher,'e', homem)
--- problemas-python-brasil/sobre_diretorio.md
+++ problemas-python-brasil/sobre_diretorio.md
@@ -4,6 +4,9 @@ Cada documento .py, do *diretório*, contém a resolução dos exercícios de ca
 
 São exercícios para iniciantes usarem a lógica de programação para resolver probemas. Uma ótima  maneira treinar a linguagem python, entre outras.
 
+É um meio que uso para praticar a linguagem e também matemática, logo os commits são resoluções de estudo dos problemas.
+
+
 
 1.  [EstruturaSequencial](https://wiki.python.org.br/EstruturaSequencial)
     
@@ -23,4 +26,4 @@ São exercícios para iniciantes usarem a lógica de programação para resolver
     
 9.  [ListaDeExerciciosProjetos](https://wiki.python.org.br/ListaDeExerciciosProjetos)
 
-**FONTE:** [**wiki.python.org.br**]([ListaDeExercicios - PythonBrasil](https://wiki.python.org.br/ListaDeExercicios))
+**FONTE:** [**PythonBrasil** --> ListaDeExercicios - wiki.python.org.br](https://wiki.python.org.br/ListaDeExercicios)
